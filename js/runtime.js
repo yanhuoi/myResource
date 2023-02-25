89f61afdcf21ab3bf7210ed55825ab98443f1daf
yanhuoi <59594053+yanhuoi@users.noreply.github.com>
2023-02-25 15:03:09 +0800

    Update runtime.js

--- js/runtime.js
+++ js/runtime.js
@@ -2,8 +2,8 @@ var now = new Date;
 
 function createtime() {
     now.setTime(now.getTime() + 1e3);
-    var e = new Date("01/01/2023 00:00:00"), t = Math.trunc(234e8 + (now - e) / 1e3 * 17), a = (t / 1496e5).toFixed(6),
-        o = new Date("08/09/2022 00:00:00"), n = (now - o) / 1e3 / 60 / 60 / 24, r = Math.floor(n),
+    var e = new Date("08/01/2022 00:00:00"), t = Math.trunc(234e8 + (now - e) / 1e3 * 17), a = (t / 1496e5).toFixed(6),
+        o = new Date("01/01/2023 00:00:00"), n = (now - o) / 1e3 / 60 / 60 / 24, r = Math.floor(n),
         i = (now - o) / 1e3 / 60 / 60 - 24 * r, s = Math.floor(i);
     1 == String(s).length && (s = "0" + s);
     var d = (now - o) / 1e3 / 60 - 1440 * r - 60 * s, l = Math.floor(d);
